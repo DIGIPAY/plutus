e08e26b9382e5bc40ce023b1ce2e70a5ae405cf0
Jopes Gallardo <jofellxcite@gmail.com>
2016-10-03 16:16:30 +0800

    Adding the model for balance log to represent the table

---
+++ app/models/plutus/balance_log.rb
@@ -0,0 +1,23 @@
+module Plutus
+  # The BalanceLog class represents the monthly opening balance of the month
+  #
+  # @abstract
+  #   A balance log must always conform with the settled balance 
+  #   of the account
+  #
+  # @author Michael Bulat
+  class BalanceLog < ActiveRecord::Base
+    belongs_to :account, :class_name => 'Plutus::Account'
+    validates_presence_of :balance, :account_id, :month_index
+
+    after_create :initialize_balance
+
+    def initialize_balance
+      month_balance = account.balance({from_date: "1970-01-01 00:00:00",
+                       to_date: "%d-%02d-01 00:00:00" % [month_index / 100,
+                                                         month_index % 100]})
+      update(balance, month_balance)
+    end
+    
+  end
+end
